7f4e3b13b588dfb28c1f059e32a23ec7ddc5017d
vannt <vannguyenthanh@admicro.vn>
2019-07-23 15:50:28 +0700

    add new file check_mysql_connection.sh

---
+++ check_mysql_connection.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#Description: Check mysql connection
+
+set -e #stop execution if a variable is no set
+
+usage() {
+  echo "Scripts check current connections on mysql:"
+  echo "Usage: $0 -w number -c number"
+}
+
+if [[ -z "$1" ]]; then
+    usage
+    exit 
+fi
+
+while getopts "w:c:" option;
+do
+  case "${option}" in
+  w) 
+    warning=${OPTARG}
+    ;;
+  c) 
+    critical=${OPTARG}
+    ;;
+  \?)
+    usage
+    exit
+    ;;
+  esac
+done
+
+#Check not number:
+number='^[0-9]+$'
+if ! [[ $2 =~ $number && $4 =~ $number ]] ; then
+    echo "Invalid option: Not a number"; 
+    usage
+    exit
+fi
+
+
+warn=$2
+crit=$4
+user=$(sed -n "2p" /root/.my.cnf|cut -d"=" -f2)
+pass=$(sed -n "3p" /root/.my.cnf|cut -d"=" -f2)
+
+mysql -u $user -p$pass -e 'select count(id) from information_schema.processlist;' > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Critical, too many connections on mysql."
+    exit 2
+fi
+
+current=$(mysql -u $user -p$pass -e 'select count(id) from information_schema.processlist;'| grep [0-9])
+if [ $current -ge $crit ]; then
+    echo "Critical! Current connections on mysql: $current"
+    exit 2
+elif [ $current -ge $warn ]; then
+    echo "Warning! Current connections on mysql: $current"
+    exit 1
+else
+    echo "OK, Current connections on mysql: $current"
+    exit 0
+fi
+
